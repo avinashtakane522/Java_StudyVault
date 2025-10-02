91ff01915930e747650be5beeee0f88e0a62b9db
Avinash Takane <takaneavinash@gmail.com>
2025-10-02 18:50:52 +0530

    My Workspace
    Demo SB created application

---
+++ Workspace/demo/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Student {
+	private int id;
+	private String name;
+}
